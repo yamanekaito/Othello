081630dd50d6576f54b074aedb3941e5bbaf97fc
yamanekaito <yamanekaito0702@gmail.com>
2020-07-10 11:29:56 +0900

    add function of turn

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
 'use strict'
 {
+  var turn = true;
+
 
   var fig = [];
   for (let i = 0; i < 8 ; i ++ ) {
     fig[i] = [];
   }
 
-  fig[4][4] = '○';
-  fig[6][2] = '●';
+  fig
 
   for (let row = 0;row < 8; row ++) {
     const tr = document.createElement('tr');
     for (let col = 0; col < 8 ; col ++ ) {
       const td = document.createElement('td');
       td.textContent = fig[col][row];
+      // 以下関数にしたい
+
+      td.addEventListener('click',() => {
+        if (turn === true) {
+          td.textContent = '●';
+          turn = false;
+        }
+      })
       tr.appendChild(td);
     }
     document.querySelector('tbody').appendChild(tr);
   }
+
+  if (turn === false ) {
+
+  }
+ 
+  // for (let row = 0;row < 8; row ++) {
+  //   for (let col = 0; col < 8 ; col ++ ) {
+  //     const a = fig[col][row]
+  //     a.addEventListener('click',() => {
+  //       if (turn === true) {
+  //         fig[col][row].textContent = '●';
+  //         turn = false;
+  //       }
+  //     })
+  //   }
+  // }
+  
   
 
-  var turn = true;
 
 }
